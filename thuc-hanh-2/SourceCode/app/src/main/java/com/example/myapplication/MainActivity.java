df33fc59ee0843590cd588285d7781c70eb2fab3
thuvan204 <nttvan204@gmail.com>
2025-05-20 17:39:13 +0700

    Bài tập thực hành

---
+++ thuc-hanh-2/SourceCode/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText etxt = findViewById(R.id.editTextTextEmailAddress);
+        Button check = findViewById(R.id.button);
+        TextView sub = findViewById(R.id.subset);
+
+        check.setOnClickListener(v -> {
+            String text = etxt.getText().toString().trim();
+
+            if (!text.contains("@")) {
+                sub.setText("Email phải chứa ký tự '@'");
+            } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
+                sub.setText("Email không hợp lệ");
+            } else {
+                sub.setText("Email hợp lệ");
+            }
+        });
+    }
+}
